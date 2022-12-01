1870e9f8b10ca169fdf40330069797a804eb0522
Kevin Nilsson <kevin7254@gmail.com>
2022-12-01 09:27:46 +0100

    day one

--- solutions/day1/program.js
+++ solutions/day1/program.js
@@ -1,3 +1,16 @@
 export default function Day(data) {
-    console.log(data);
+    let num = 0;
+    const arr = [];
+    data.forEach(element => {
+        if (element) {
+            num += parseInt(element);
+        } else {
+            arr.push(num);
+            num = 0;
+        }
+    });
+    arr.sort((a, b) => a - b);
+    console.log("Part one: " + arr[arr.length - 1]);
+    const sum = arr.slice(-3).reduce((acc, val) => acc + val);
+    console.log("Part two: " + sum)
 }
